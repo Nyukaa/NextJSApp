508e02fa04b750d041b206b33f41f316eb88b684
Nyukaa <annashitikova@gmail.com>
2026-03-27 11:57:47 +0200

    Add Header component, update layout and metadata, and create About and Dashboard pages

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,9 @@
-//import Image from "next/image";
-
-export default function Home() {
+const DashboardPage = () => {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      App will be here
+      Dashboard will be Header
     </div>
   );
-}
+};
+
+export default DashboardPage;
