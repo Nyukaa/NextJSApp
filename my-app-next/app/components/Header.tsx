1d10c79312df8a1730662f9933839410d0b78a9e
Nyukaa <annashitikova@gmail.com>
2026-04-01 11:10:15 +0300

    Add my-app-next project

---
+++ my-app-next/app/components/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header className="w-full h-16 bg-zinc-900 text-white flex items-center justify-center">
+      <h1 className="text-xl font-bold">My Next.js Header</h1>
+    </header>
+  );
+};
+export default Header;
